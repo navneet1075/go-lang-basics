96e5f4dcf4b421d6278aaf92ab0a2f9ec62f4fb9
navneet <navneet1075@gmail.com>
2019-12-15 15:39:58 +0800

    move insertintomap into init method

---
+++ webservermux.go
@@ -0,0 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"gorilla/mux"
+// )
+
+// //Article describes a article entity
+// type Article struct {
+// 	Name string `json:"name"`
+// 	Desc string `json:"desc"`
+// }
+
+// //Version defines the application current version
+// type Version struct {
+// 	Version int `json:"version"`
+// }
+
+// func getVersion(response http.ResponseWriter, request *http.Request) {
+// 	fmt.Fprintf(response, "%v\n", Version{Version: 1})
+// }
+// func getArticles(response http.ResponseWriter, request *http.Request) {
+
+// 	articles := []Article{
+// 		Article{Name: "Sample", Desc: "Sample Description"},
+// 	}
+// 	fmt.Fprintf(response, "%v\n", articles)
+// }
+
+// func registerHandlerForApplication() {
+
+// 	myRouter := mux.New
+// 	http.HandleFunc("/version", getVersion)
+// 	http.HandleFunc("/articles", getArticles)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+// func main() {
+// 	registerHandlerForApplication()
+// }
